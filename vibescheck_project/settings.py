8f4dab4c097cb912e6dafc607a50eda011ac9c9c
tiffchannn <tiffany.yi.chan@gmail.com>
2021-03-09 12:13:03 -0800

    Updated settings.py to congifure Heroku deployment

--- vibescheck_project/settings.py
+++ vibescheck_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i0bxrn%wvgx30o@*gsx%2awn^c-itxiq9h3+ygd4*szzg*x_5p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vibescheck.herokuapp.com']
 
@@ -120,10 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
 # Test
+
+django_heroku.settings(locals())
